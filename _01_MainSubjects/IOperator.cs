d128ce3f7a3230bfa7683f7167d4bde6fade29a3
krmn-brc <136928384+krmn-brc@users.noreply.github.com>
2024-10-27 19:09:16 +0000

    Change Project

--- _01_MainSubjects/IOperator.cs
+++
@@ -1,9 +0,0 @@
-
-
-namespace _01_MainSubjects
-{
-    public interface IOperator
-    {
-        void Notify(TransactionType transactionType, string message);
-    }
-}
--- _01_MainSubjects/TransactionType.cs
+++
@@ -1,7 +0,0 @@
-namespace _01_MainSubjects
-{
-    public enum TransactionType
-    {
-        
-    }
-}
